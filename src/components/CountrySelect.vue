be8e1fbff726a4fd37b8fe777876e600834972fd
KevSeenan <kev.seenan@gmail.com>
2019-08-28 20:59:14 +0100

    Adds search bar and button but needs functionality

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,11 +1,17 @@
 <template lang="html">
   <div>
-      <label for="country-select">Select A Country:</label>
-      <select v-model="country" v-on:change="handleChange()">
-        <option disabled value="">Select A Country</option>
-        <option v-for="(country, index) in countries" :value="country" :key="index">{{country.name}}</option>
-      </select>
-    </div>
+    <label for="country-select">Select A Country:</label>
+    <select v-model="country" v-on:change="handleChange">
+      <option disabled value="">Select A Country</option>
+      <option v-for="(country, index) in countries" :value="country" :key="index">{{country.name}}</option>
+    </select>
+
+    <label for="site-search">Search Countries:</label>
+    <input type="search" id="site-search">
+
+    <button>Search</button>
+
+  </div>
 </select>
 </template>
 
