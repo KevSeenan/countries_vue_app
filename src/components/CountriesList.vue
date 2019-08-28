4f9f8c52ba72c31357e035ed8329cca2641bc809
KevSeenan <kev.seenan@gmail.com>
2019-08-28 17:05:20 +0100

    Adds Completed detail on selected country - from paired lab with Allan

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div>
+    <!-- <label for="country-select">Select A Country:</label>
+    <select id="country-select" v-model="selectedCountry">
+      <option disabled value="">Select A Country</option>
+      <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
+    </select> -->
+
+    <ul>
+      <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+import {eventBus} from '../main.js';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  // data(){
+  //   return {
+  //     selectedCountry: null
+  //   }
+  // },
+  components: {
+    "list-item": ListItem
+  },
+  methods: {
+    onChange(event){
+      eventBus.$emit('country-selected', this.country);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+dropo down menu in here
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
